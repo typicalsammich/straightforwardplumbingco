61c0ec0bcf926e29968dbc7cf99c73fb57317e01
typicalsammich <pansean777@gmail.com>
2026-09-25 18:08:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.querySelector('.menu')?.addEventListener('click',()=>document.querySelector('.links')?.classList.toggle('open'));
+const menu=document.querySelector('.menu');
+menu?.addEventListener('click',()=>document.querySelector('.links')?.classList.toggle('open'));
+const mobileCall=document.querySelector('.mobile-call');
+if(mobileCall){
+  const syncCall=()=>mobileCall.classList.toggle('is-visible',window.scrollY>220);
+  syncCall();
+  window.addEventListener('scroll',syncCall,{passive:true});
+}
